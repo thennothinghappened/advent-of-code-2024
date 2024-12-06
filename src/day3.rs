f661951bcd671a184a57abe9a85b1967434d6077
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2024-12-06 11:48:10 +1100

    feat: day 3 pt 1 & 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,4 +1,4 @@
-use std::{char::MAX, cmp, error::Error, f32::MIN, ops::Deref};
+use std::error::Error;
 
 use crate::not_yet_implemented;
 
@@ -7,9 +7,95 @@ pub(crate) fn solve(input: &str) -> Result<(String, String), Box<dyn Error>> {
 }
 
 fn part1(input: &str) -> Result<String, Box<dyn Error>> {
-	Ok(not_yet_implemented())
+    let mut rem_input = input;
+    let mut sum = 0;
+
+    loop {
+        let Some(next_match) = rem_input.find("mul(") else {
+            break;
+        };
+
+        rem_input = &rem_input[next_match + 4..];
+
+        let Some(comma) = rem_input.find(",") else {
+            continue;
+        };
+
+        let Some(closing) = rem_input.find(")") else {
+            continue;
+        };
+
+        if comma >= closing {
+            continue;
+        }
+
+        let Ok(lhs) = rem_input[..comma].parse::<i32>() else {
+            continue;
+        };
+
+        let Ok(rhs) = rem_input[comma + 1..closing].parse::<i32>() else {
+            continue;
+        };
+
+        println!("Match :: {} * {}", lhs, rhs);
+        sum += lhs * rhs;
+    }
+
+    Ok(sum.to_string())
 }
 
 fn part2(input: &str) -> Result<String, Box<dyn Error>> {
-    Ok(not_yet_implemented())
+    let mut rem_input = input;
+	let mut bother = true;
+    let mut sum = 0;
+
+    loop {
+        let Some(next_match) = rem_input.find("mul(") else {
+            break;
+        };
+
+		match bother {
+			true => if let Some(next_dont) = rem_input.find("don't()") {
+				if next_dont < next_match {
+					bother = false;
+				}
+			},
+			false => if let Some(next_do) = rem_input.find("do()") {
+				if next_do < next_match {
+					bother = true;
+				}
+			},
+		}
+
+        rem_input = &rem_input[next_match + 4..];
+
+		if !bother {
+			continue;
+		}
+
+        let Some(comma) = rem_input.find(",") else {
+            continue;
+        };
+
+        let Some(closing) = rem_input.find(")") else {
+            continue;
+        };
+
+        if comma >= closing {
+            continue;
+        }
+
+        let Ok(lhs) = rem_input[..comma].parse::<i32>() else {
+            continue;
+        };
+
+        let Ok(rhs) = rem_input[comma + 1..closing].parse::<i32>() else {
+            continue;
+        };
+
+        println!("Match :: {} * {}", lhs, rhs);
+        sum += lhs * rhs;
+    }
+
+    Ok(sum.to_string())
 }
