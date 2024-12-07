0003a1597d457f5f231c4508f8b63cb3bad18a88
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2024-12-07 21:23:56 +1100

    refactor: use par_iter & prefer functional style

--- src/days/day7.rs
+++ src/days/day7.rs
@@ -76,41 +76,33 @@ fn part1(equations: &Vec<Equation>) -> PartResult {
 
 fn part2(equations: &Vec<Equation>) -> PartResult {
     let sum: usize = equations
-        .iter()
+        .par_iter()
         .filter_map(|equation| {
             let num_operators = equation.operands.len() - 1;
-            let num_combos = 2_usize.pow(num_operators as u32);
 
-            println!("-------------");
-            println!(
-                "Attempt :: Desired Result = {} from operands {:?} (#possible combos = {})",
-                equation.result, equation.operands, num_combos
-            );
-
-            for combo in (0..num_operators)
+            (0..num_operators)
                 .map(|_| [Op::Add, Op::Mul, Op::Concat])
                 .multi_cartesian_product()
-            {
-                let mut sum = equation.operands[0];
+                .find_map(|combo| {
+                    let mut sum = equation.operands[0];
 
-                for op_index in 0..num_operators {
-                    if sum > equation.result {
-                        break;
+                    for op_index in 0..num_operators {
+                        if sum > equation.result {
+                            break;
+                        }
+
+                        let rhs = equation.operands[op_index + 1];
+                        let op = combo[op_index];
+
+                        sum = op.perform(sum, rhs);
                     }
 
-                    let rhs = equation.operands[op_index + 1];
-                    let op = combo[op_index];
+                    if sum == equation.result {
+                        return Some(equation.result);
+                    }
 
-                    sum = op.perform(sum, rhs);
-                }
-
-                if sum == equation.result {
-                    println!("Success! :: Using combination {:?}", combo);
-                    return Some(equation.result);
-                }
-            }
-
-            None
+                    None
+                })
         })
         .sum();
 
