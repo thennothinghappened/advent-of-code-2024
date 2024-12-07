ecbde2a9cfac365b51aece998439ccb358c705ea
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2024-12-07 13:07:22 +1100

    feat: optimise d6 with flat vecs

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -1,4 +1,7 @@
-use std::ops::{Index, IndexMut};
+use std::{
+    ops::{Index, IndexMut},
+    path::Display,
+};
 
 use enumflags2::BitFlags;
 
@@ -7,29 +10,32 @@ use super::{DayResult, PartResult};
 pub(crate) fn solve(input: &str) -> DayResult {
     let mut grid = input
         .lines()
-        .map(|line| line.chars().collect::<Vec<char>>())
+        .flat_map(|line| line.chars())
         .collect::<Vec<_>>();
+
+    let grid_width = input
+        .lines()
+        .next()
+        .expect("Grid shouldn't be empty...")
+        .len();
+
+    let grid_height = grid.len() / grid_width;
 
     let initial_dir = Direction::Up;
     let initial_pos = grid
         .iter()
         .enumerate()
-        .find_map(|(y, row)| {
-            row.iter().enumerate().find_map(|(x, &c)| match c {
-                GUARD_INITIAL => Some(Pos {
-                    x: x as i32,
-                    y: y as i32,
-                }),
-                _ => None,
-            })
+        .find_map(|(i, &col)| match col {
+            GUARD_INITIAL => Some(Pos::from_flat_index(grid_width, i)),
+            _ => None,
         })
         .expect("Guard must have an initial position!");
 
-    grid[initial_pos] = FLOOR;
+    *grid.flat_index_mut(grid_width, initial_pos) = FLOOR;
 
     Ok((
-        part1(&grid, initial_pos, initial_dir)?,
-        part2(&mut grid, initial_pos, initial_dir)?,
+        part1(&grid, grid_width, grid_height, initial_pos, initial_dir)?,
+        part2(&mut grid, grid_width, grid_height, initial_pos, initial_dir)?,
     ))
 }
 
@@ -37,47 +43,58 @@ const FLOOR: char = '.';
 const WALL: char = '#';
 const GUARD_INITIAL: char = '^';
 
-fn part1(grid: &Vec<Vec<char>>, initial_pos: Pos, initial_dir: Direction) -> PartResult {
-    let mut visit_grid = grid
-        .iter()
-        .map(|row| std::vec::from_elem(BitFlags::<Direction>::empty(), row.len()))
-        .collect::<Vec<_>>();
-
-    let visited = trace_path(&grid, &mut visit_grid, initial_pos, initial_dir)
-        .expect("Somehow there was no valid exit!");
+fn part1(
+    grid: &Vec<char>,
+    grid_width: usize,
+    grid_height: usize,
+    initial_pos: Pos,
+    initial_dir: Direction,
+) -> PartResult {
+    let mut visit_grid = vec![BitFlags::<Direction>::empty(); grid_width * grid_height];
+    let visited = trace_path(
+        &grid,
+        grid_width,
+        grid_height,
+        &mut visit_grid,
+        initial_pos,
+        initial_dir,
+    )
+    .expect("Somehow there was no valid exit!");
 
     Ok(visited.to_string())
 }
 
-fn part2(grid: &mut Vec<Vec<char>>, initial_pos: Pos, initial_dir: Direction) -> PartResult {
+fn part2(
+    grid: &mut Vec<char>,
+    grid_width: usize,
+    grid_height: usize,
+    initial_pos: Pos,
+    initial_dir: Direction,
+) -> PartResult {
     // 1. Let's map out their path as in part 1.
     // 2. For each visited position, try placing a wall there.
     // 3. Record the pathfinding, rather than an X, store direction (bitwise mayhaps :P)
     // 4. If we've travelled the same position twice in the same direction we've made a loop.
     // 5. Output #loops.
 
-    let mut visit_grid = grid
-        .iter()
-        .map(|row| std::vec::from_elem(BitFlags::<Direction>::empty(), row.len()))
-        .collect::<Vec<_>>();
-
-    trace_path(&grid, &mut visit_grid, initial_pos, initial_dir);
+    let mut visit_grid = vec![BitFlags::<Direction>::empty(); grid_width * grid_height];
+    trace_path(
+        &grid,
+        grid_width,
+        grid_height,
+        &mut visit_grid,
+        initial_pos,
+        initial_dir,
+    );
 
     let targets = visit_grid
         .iter()
         .enumerate()
-        .flat_map(|(y, row)| {
-            row.iter()
-                .enumerate()
-                .filter_map(move |(x, col)| match col.is_empty() {
-                    true => None,
-                    false => Some(Pos {
-                        x: x as i32,
-                        y: y as i32,
-                    }),
-                })
-                .filter(|&pos| pos != initial_pos)
+        .filter_map(|(i, col)| match col.is_empty() {
+            true => None,
+            false => Some(Pos::from_flat_index(grid_width, i)),
         })
+        .filter(|&pos| pos != initial_pos)
         .collect::<Vec<Pos>>();
 
     let mut valid_targets = 0;
@@ -86,15 +103,22 @@ fn part2(grid: &mut Vec<Vec<char>>, initial_pos: Pos, initial_dir: Direction) ->
         // Refresh the visit positions grid.
         visit_grid
             .iter_mut()
-            .for_each(|row| row.iter_mut().for_each(|col| *col = BitFlags::empty()));
+            .for_each(|col| *col = BitFlags::empty());
 
-        grid[target] = WALL;
+        *grid.flat_index_mut(grid_width, target) = WALL;
 
-        if let None = trace_path(&grid, &mut visit_grid, initial_pos, initial_dir) {
+        if let None = trace_path(
+            &grid,
+            grid_width,
+            grid_height,
+            &mut visit_grid,
+            initial_pos,
+            initial_dir,
+        ) {
             valid_targets += 1;
         }
 
-        grid[target] = FLOOR;
+        *grid.flat_index_mut(grid_width, target) = FLOOR;
     }
 
     Ok(valid_targets.to_string())
@@ -104,8 +128,10 @@ fn part2(grid: &mut Vec<Vec<char>>, initial_pos: Pos, initial_dir: Direction) ->
 /// `visit_grid`. If the guard exited the map, the number of steps is returned. If they got stuck in
 /// a loop however, [None](None) is returned.
 fn trace_path(
-    grid: &Vec<Vec<char>>,
-    visit_grid: &mut Vec<Vec<BitFlags<Direction>>>,
+    grid: &Vec<char>,
+    grid_width: usize,
+    grid_height: usize,
+    visit_grid: &mut Vec<BitFlags<Direction>>,
     initial_pos: Pos,
     initial_dir: Direction,
 ) -> Option<usize> {
@@ -113,29 +139,56 @@ fn trace_path(
     let mut dir = initial_dir;
     let mut visit_count = 1;
 
-    let grid_width = grid[pos.y as usize].len() as i32;
-    let grid_height = grid.len() as i32;
-
     loop {
-        if visit_grid[pos].contains(dir) {
+        // println!(
+        //     "{}\nGuard :: (x: {}, y: {}) :: facing {:?} :: Spot contains {}\n",
+        //     grid.chunks(grid_width)
+        //         .zip(visit_grid.chunks(grid_width))
+        //         .map(|(map_row, visits_row)| {
+        //             let mut str = String::new();
+
+        //             for i in 0..grid_width {
+        //                 let visits_col = visits_row[i];
+
+        //                 if !visits_col.is_empty() {
+        //                     str.push('X');
+        //                 } else {
+        //                     str.push(map_row[i]);
+        //                 }
+        //             }
+
+        //             str
+        //         })
+        //         .collect::<Vec<String>>()
+        //         .join("\n"),
+        //     pos.x,
+        //     pos.y,
+        //     dir,
+        //     visit_grid.flat_index(grid_width, pos)
+        // );
+
+        if visit_grid.flat_index(grid_width, pos).contains(dir) {
             // We've been here before!
             return None;
         }
 
+        *visit_grid.flat_index_mut(grid_width, pos) |= dir;
         let next_pos = pos + dir;
-        visit_grid[pos] |= dir;
 
-        if !next_pos.is_positive() || next_pos.y >= grid_height || next_pos.x >= grid_width {
+        if !next_pos.is_positive()
+            || next_pos.y as usize >= grid_height
+            || next_pos.x as usize >= grid_width
+        {
             // Exiting the map.
             break;
         }
 
-        if grid[next_pos] == WALL {
+        if *grid.flat_index(grid_width, next_pos) == WALL {
             dir = dir.turned_right();
             continue;
         }
 
-        if visit_grid[next_pos].is_empty() {
+        if visit_grid.flat_index(grid_width, next_pos).is_empty() {
             visit_count += 1;
         }
 
@@ -154,6 +207,16 @@ struct Pos {
 impl Pos {
     fn is_positive(&self) -> bool {
         self.x >= 0 && self.y >= 0
+    }
+
+    fn from_flat_index(grid_width: usize, index: usize) -> Self {
+        let x = index % grid_width;
+        let y = (index - x) / grid_width;
+
+        Pos {
+            x: x as i32,
+            y: y as i32,
+        }
     }
 }
 
@@ -185,6 +248,12 @@ impl From<Pos> for (i32, i32) {
 impl From<(i32, i32)> for Pos {
     fn from((x, y): (i32, i32)) -> Self {
         Pos { x, y }
+    }
+}
+
+impl std::fmt::Display for Pos {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "(x: {}, y: {})", self.x, self.y)
     }
 }
 
@@ -231,16 +300,25 @@ impl From<Direction> for char {
     }
 }
 
-impl<T> Index<Pos> for Vec<Vec<T>> {
+trait FlatIndex<T> {
+    type Output: ?Sized;
+    fn flat_index(&self, width: usize, index: T) -> &Self::Output;
+}
+
+trait FlatIndexMut<T>: FlatIndex<T> {
+    fn flat_index_mut(&mut self, width: usize, index: T) -> &mut Self::Output;
+}
+
+impl<T> FlatIndex<Pos> for Vec<T> {
     type Output = T;
 
-    fn index(&self, index: Pos) -> &Self::Output {
-        &self[index.y as usize][index.x as usize]
+    fn flat_index(&self, width: usize, index: Pos) -> &Self::Output {
+        &self[index.x as usize + (index.y as usize) * width]
     }
 }
 
-impl<T> IndexMut<Pos> for Vec<Vec<T>> {
-    fn index_mut(&mut self, index: Pos) -> &mut Self::Output {
-        &mut self[index.y as usize][index.x as usize]
+impl<T> FlatIndexMut<Pos> for Vec<T> {
+    fn flat_index_mut(&mut self, width: usize, index: Pos) -> &mut Self::Output {
+        &mut self[index.x as usize + (index.y as usize) * width]
     }
 }
