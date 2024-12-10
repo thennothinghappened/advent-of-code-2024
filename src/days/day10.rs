f6763b1e8f4eb3e9cede39bc075f469654b15140
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2024-12-10 20:25:41 +1100

    feat: d10p1

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -1,6 +1,14 @@
-use itertools::Itertools;
+use std::{iter, vec};
 
-use crate::utils::not_yet_implemented;
+use itertools::Itertools;
+use rayon::iter::empty;
+use rustc_hash::FxHashSet;
+
+use crate::utils::{
+    direction::DIRECTIONS,
+    not_yet_implemented,
+    pos::{Index2d, Pos},
+};
 
 use super::{DayResult, PartResult};
 
@@ -9,7 +17,55 @@ pub(crate) fn solve(input: &str) -> DayResult {
 }
 
 fn part1(input: &str) -> PartResult {
-    not_yet_implemented()
+    let grid = input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|char| char.to_digit(10).unwrap())
+                .collect_vec()
+        })
+        .collect_vec();
+
+    let mut seen_peaks = FxHashSet::<Pos>::default();
+    let mut sum = 0;
+
+    for (y, row) in grid.iter().enumerate() {
+        for (x, _) in row.iter().enumerate().filter(|(_, col)| **col == 0) {
+            let start_pos = Pos::new_from_usize_unchecked(x, y);
+            let peaks = find_peaks(&grid, start_pos, 0);
+
+            seen_peaks.clear();
+
+            for peak in peaks {
+                seen_peaks.insert(peak);
+            }
+
+            sum += seen_peaks.len();
+        }
+    }
+
+    Ok(sum.to_string())
+}
+
+fn find_peaks(grid: &Vec<Vec<u32>>, from_pos: Pos, from_height: u32) -> Vec<Pos> {
+    DIRECTIONS
+        .iter()
+        .filter_map(move |&direction| {
+            let pos = from_pos + direction;
+            let height = grid.get_2d(pos)?;
+
+            if *height - 1 != from_height {
+                return None;
+            }
+
+            if *height == 9 {
+                return Some(vec![pos]);
+            }
+
+            Some(find_peaks(grid, pos, *height))
+        })
+        .flatten()
+        .collect()
 }
 
 fn part2(input: &str) -> PartResult {
