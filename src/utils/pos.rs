3e95fa46536f6f1653421c0e495b817fcc861276
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2024-12-10 17:14:25 +1100

    feat: index 2d vec with `Pos`

--- src/utils/pos.rs
+++ src/utils/pos.rs
@@ -1,3 +1,5 @@
+use std::{ops::Index, slice::SliceIndex};
+
 #[derive(PartialEq, Eq, Debug, Clone, Copy, Hash)]
 pub struct Pos {
     pub x: i32,
@@ -7,6 +9,17 @@ pub struct Pos {
 impl Pos {
     pub fn is_positive(&self) -> bool {
         self.x >= 0 && self.y >= 0
+    }
+
+    pub fn is_in_rect(&self, top_left: &Pos, bottom_right: &Pos) -> bool {
+        self.x >= top_left.x
+            && self.y >= top_left.y
+            && self.x <= bottom_right.x
+            && self.y <= bottom_right.y
+    }
+
+    pub fn is_valid_grid_index(&self, grid_width: usize, grid_height: usize) -> bool {
+        self.is_positive() && (self.x as usize) < grid_width && (self.y as usize) < grid_height
     }
 
     pub fn from_flat_index(grid_width: usize, index: usize) -> Self {
@@ -56,5 +69,20 @@ impl std::ops::Mul for Pos {
 impl From<i32> for Pos {
     fn from(value: i32) -> Self {
         Pos { x: value, y: value }
+    }
+}
+
+/// Rust did not want to cooperate with me implementing `Index` to use with `Vec`.
+trait Index2<T> {
+    type Output;
+
+    fn index(&self, index: Pos) -> &Self::Output;
+}
+
+impl<T> Index2<Pos> for [&Vec<T>] {
+    type Output = T;
+
+    fn index(&self, index: Pos) -> &Self::Output {
+        &self[index.y as usize][index.x as usize]
     }
 }
