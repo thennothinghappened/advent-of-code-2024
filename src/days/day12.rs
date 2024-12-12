9b390332e147d2e3059cd42d17198f622e17b289
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2024-12-12 17:51:47 +1100

    feat: d12p1

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,4 +1,10 @@
-use crate::utils::not_yet_implemented;
+use itertools::Itertools;
+
+use crate::utils::{
+    direction::{Direction, DIRECTIONS},
+    not_yet_implemented,
+    pos::{Index2d, Pos},
+};
 
 use super::{DayResult, PartResult};
 
@@ -7,9 +13,81 @@ pub(crate) fn solve(input: &str) -> DayResult {
 }
 
 fn part1(input: &str) -> PartResult {
-    not_yet_implemented()
+    let mut grid = input
+        .lines()
+        .map(|line| line.chars().map(Plant::new).collect_vec())
+        .collect_vec();
+
+    let grid_height = grid.len();
+    let grid_width = grid[0].len();
+
+    let mut sum: u64 = 0;
+
+    for y in 0..grid_height {
+        for x in 0..grid_width {
+            let pos = Pos::new_from_usize_unchecked(x, y);
+            let plant = *grid.get_2d_unchecked(pos);
+
+            if plant.seen {
+                continue;
+            }
+
+            let mut perimeter = 0;
+            let mut area = 0;
+
+            define_region(&mut grid, plant.species, pos, &mut perimeter, &mut area);
+
+            sum += area * perimeter;
+        }
+    }
+
+    Ok(sum.to_string())
 }
 
 fn part2(input: &str) -> PartResult {
     not_yet_implemented()
+}
+
+fn define_region(
+    grid: &mut Vec<Vec<Plant>>,
+    species: char,
+    pos: Pos,
+    perimeter: &mut u64,
+    area: &mut u64,
+) {
+    let Some(plant) = grid.get_2d_mut(pos) else {
+        *perimeter += 1;
+        return;
+    };
+
+    if plant.species != species {
+        *perimeter += 1;
+        return;
+    }
+
+    if plant.seen {
+        return;
+    }
+
+    *area += 1;
+    plant.seen = true;
+
+    for direction in DIRECTIONS {
+        define_region(grid, species, pos + direction, perimeter, area);
+    }
+}
+
+#[derive(Clone, Copy)]
+struct Plant {
+    species: char,
+    seen: bool,
+}
+
+impl Plant {
+    fn new(species: char) -> Self {
+        Plant {
+            species,
+            seen: false,
+        }
+    }
 }
