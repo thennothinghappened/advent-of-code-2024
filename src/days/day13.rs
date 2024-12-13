6d5ca18f35cfbd6ee5f0c26e659c3a58ff1ff5c1
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2024-12-13 16:39:53 +1100

    feat: d13p1 (ew bruteforce)

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -8,8 +8,9 @@ pub(crate) fn solve(input: &str) -> DayResult {
     Ok((part1(input)?, part2(input)?))
 }
 
-const A_COST: u64 = 3;
-const B_COST: u64 = 1;
+const A_COST: i32 = 3;
+const B_COST: i32 = 1;
+const MAX_PRESSES: i32 = 100;
 
 fn part1(input: &str) -> PartResult {
     let machines = input
@@ -32,7 +33,34 @@ fn part1(input: &str) -> PartResult {
             },
         });
 
-    not_yet_implemented()
+    let mut spent_tokens = 0;
+
+    for machine in machines {
+        let mut cheapest = (i32::MAX, i32::MAX);
+
+        for a_presses in 0..=MAX_PRESSES {
+            for b_presses in 0..=MAX_PRESSES {
+                if (machine.a * a_presses + machine.b * b_presses) == machine.prize {
+                    cheapest.0 = a_presses;
+                    cheapest.1 = b_presses;
+                    break;
+                }
+            }
+        }
+
+        if cheapest.0 <= MAX_PRESSES && cheapest.1 <= MAX_PRESSES {
+            spent_tokens += cheapest.0 * A_COST + cheapest.1 * B_COST;
+        //     println!(
+        //         "{:?} :: Cheapest solution costs {} tokens.",
+        //         machine,
+        //         cheapest.0 * A_COST + cheapest.1 * B_COST
+        //     );
+        } else {
+            //     println!("{:?} :: No solutions found.", machine);
+        }
+    }
+
+    Ok(spent_tokens.to_string())
 }
 
 fn part2(input: &str) -> PartResult {
