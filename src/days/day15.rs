27830c313f8d85ee883e3a57621180db565aa012
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2024-12-15 17:22:37 +1100

    feat: d15p2 horizontal movement

--- src/days/day15.rs
+++ src/days/day15.rs
@@ -92,7 +92,102 @@ fn part1(input: &str) -> PartResult {
 }
 
 fn part2(input: &str) -> PartResult {
-    not_yet_implemented()
+    let mut robot_pos = Pos { x: 0, y: 0 };
+
+    let mut lines = input.lines();
+    let mut grid = lines
+        .by_ref()
+        .take_while(|line| !line.is_empty())
+        .enumerate()
+        .map(|(y, line)| {
+            line.chars()
+                .enumerate()
+                .flat_map(|(x, char)| match char {
+                    '.' => [DWTile::Air, DWTile::Air],
+                    '#' => [DWTile::Wall, DWTile::Wall],
+                    'O' => [DWTile::BoxLeft, DWTile::BoxRight],
+                    '@' => {
+                        robot_pos = Pos::new_from_usize_unchecked(x * 2, y);
+                        [DWTile::Air, DWTile::Air]
+                    }
+                    _ => panic!("Invalid char in grid!"),
+                })
+                .collect_vec()
+        })
+        .collect_vec();
+
+    for move_dir in lines.join("").chars().map(|char| match char {
+        '^' => Direction::Up,
+        '>' => Direction::Right,
+        'v' => Direction::Down,
+        '<' => Direction::Left,
+        _ => panic!("Invalid direction to move!"),
+    }) {
+        let target = robot_pos + move_dir;
+        let target_tile = *grid.get_2d_unchecked(target);
+
+        debug_show_state(&grid, robot_pos);
+        println!("Next Move: {:?}", move_dir);
+        wait_for_user();
+
+        match target_tile {
+            DWTile::Air => {
+                robot_pos = target;
+                continue;
+            }
+            DWTile::Wall => {
+                continue;
+            }
+            _ => (),
+        }
+
+        if move_dir.is_horizontal() {
+            // we've gotta actually move the whole row across this time!
+            let Some(free) = p2_find_air_horizontal(&grid, target, move_dir) else {
+                continue;
+            };
+
+            *grid.get_2d_mut_unchecked(target) = DWTile::Air;
+            *grid.get_2d_mut_unchecked(free) = if move_dir == Direction::Left {
+                DWTile::BoxLeft
+            } else {
+                DWTile::BoxRight
+            };
+
+            for x in if free.x > target.x {
+                (target.x + 1)..free.x
+            } else {
+                (free.x + 1)..target.x
+            } {
+                let edit_pos = Pos { x, y: target.y };
+                *grid.get_2d_mut_unchecked(edit_pos) = match grid.get_2d_unchecked(edit_pos) {
+                    DWTile::BoxLeft => DWTile::BoxRight,
+                    DWTile::BoxRight => DWTile::BoxLeft,
+                    _ => panic!("This should be unreachable!"),
+                };
+            }
+        } else {
+            // shenanigans afoot!
+            todo!("aaa!!!");
+        }
+
+        robot_pos = target;
+    }
+
+    let gps_sum = grid
+        .iter()
+        .enumerate()
+        .flat_map(|(y, row)| {
+            row.iter()
+                .enumerate()
+                .filter_map(move |(x, tile)| match tile {
+                    DWTile::BoxLeft => Some(x + (100 * y)),
+                    _ => None,
+                })
+        })
+        .sum::<usize>();
+
+    Ok(gps_sum.to_string())
 }
 
 fn find_air_in_direction(
@@ -117,7 +212,34 @@ fn find_air_in_direction(
     None
 }
 
-fn debug_show_state(grid: &[Vec<Tile>], robot_pos: Pos) {
+fn p2_find_air_horizontal(
+    grid: &Vec<Vec<DWTile>>,
+    start_pos: Pos,
+    direction: Direction,
+) -> Option<Pos> {
+    assert!(direction.is_horizontal());
+
+    for offset in 1..i32::MAX {
+        let pos = start_pos + (Pos::from(direction) * offset);
+
+        let Some(tile) = grid.get_2d(pos) else {
+            break;
+        };
+
+        match tile {
+            DWTile::Air => return Some(pos),
+            DWTile::Wall => break,
+            _ => continue,
+        }
+    }
+
+    None
+}
+
+fn debug_show_state<T: Copy>(grid: &[Vec<T>], robot_pos: Pos)
+where
+    char: From<T>,
+{
     println!(
         "\n{}",
         grid.iter()
@@ -143,12 +265,31 @@ enum Tile {
     Box,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum DWTile {
+    Air,
+    Wall,
+    BoxLeft,
+    BoxRight,
+}
+
 impl From<Tile> for char {
     fn from(value: Tile) -> Self {
         match value {
             Tile::Air => '.',
             Tile::Wall => '#',
             Tile::Box => 'O',
+        }
+    }
+}
+
+impl From<DWTile> for char {
+    fn from(value: DWTile) -> Self {
+        match value {
+            DWTile::Air => '.',
+            DWTile::Wall => '#',
+            DWTile::BoxLeft => '[',
+            DWTile::BoxRight => ']',
         }
     }
 }
