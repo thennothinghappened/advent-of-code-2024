92c0c8ee311e6252c8b0e4f13d008ad94de82532
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2024-12-16 15:59:17 +1100

    feat: d16 template

--- src/days.rs
+++ src/days.rs
@@ -5,6 +5,7 @@ mod day12;
 mod day13;
 mod day14;
 mod day15;
+mod day16;
 mod day2;
 mod day3;
 mod day4;
@@ -37,4 +38,5 @@ pub(crate) const DAYS: &[DayFunc] = &[
     day13::solve,
     day14::solve,
     day15::solve,
+    day16::solve,
 ];
