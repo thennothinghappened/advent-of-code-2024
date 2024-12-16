360e7417bb338934f4fb9b4ab9f66ba0bc0de0e2
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2024-12-16 16:05:27 +1100

    feat: d16 grid parsing

--- src/days/day16.rs
+++ src/days/day16.rs
@@ -1,4 +1,9 @@
-use crate::utils::not_yet_implemented;
+use itertools::Itertools;
+
+use crate::utils::{
+    boxdraw, not_yet_implemented,
+    pos::{Index2d, Pos},
+};
 
 use super::{DayResult, PartResult};
 
@@ -7,9 +12,50 @@ pub(crate) fn solve(input: &str) -> DayResult {
 }
 
 fn part1(input: &str) -> PartResult {
+    let mut start_pos = Pos::from(0);
+    let mut exit_pos = Pos::from(0);
+
+    let grid = input
+        .lines()
+        .enumerate()
+        .map(|(y, line)| {
+            line.chars()
+                .enumerate()
+                .map(|(x, char)| match char {
+                    '.' => Tile::Air,
+                    '#' => Tile::Wall,
+                    'S' => {
+                        start_pos = Pos::new_from_usize_unchecked(x, y);
+                        Tile::Air
+                    }
+                    'E' => {
+                        exit_pos = Pos::new_from_usize_unchecked(x, y);
+                        Tile::Air
+                    }
+                    _ => panic!("Invalid char in grid!"),
+                })
+                .collect_vec()
+        })
+        .collect_vec();
+
+    println!(
+        "{}",
+        boxdraw::draw_shape(grid[0].len(), grid.len(), |pos| {
+            match grid.get_2d_unchecked(pos) {
+                Tile::Air => false,
+                Tile::Wall => true,
+            }
+        })
+    );
+
     not_yet_implemented()
 }
 
 fn part2(input: &str) -> PartResult {
     not_yet_implemented()
+}
+
+enum Tile {
+    Air,
+    Wall,
 }
