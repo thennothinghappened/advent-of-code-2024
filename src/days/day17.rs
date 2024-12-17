60112e26643bc23c7df927f3af4e63063c56f57b
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2024-12-17 16:19:18 +1100

    feat: parse machine state d17

--- src/days/day17.rs
+++ src/days/day17.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 use crate::utils::not_yet_implemented;
 
 use super::{DayResult, PartResult};
@@ -7,6 +9,24 @@ pub(crate) fn solve(input: &str) -> DayResult {
 }
 
 fn part1(input: &str) -> PartResult {
+    let mut lines = input.lines();
+
+    let mut a: i32 = lines.next().unwrap()[12..].parse().unwrap();
+    let mut b: i32 = lines.next().unwrap()[12..].parse().unwrap();
+    let mut c: i32 = lines.next().unwrap()[12..].parse().unwrap();
+
+    lines.next();
+
+    for (opcode, operand) in lines.next().unwrap()[9..]
+        .split(",")
+        .map(|num| num.parse::<i32>().unwrap())
+        .chunks(2)
+        .into_iter()
+        .map(|chunk| chunk.collect_tuple().unwrap())
+    {
+        println!("Opcode {} with operand {}", opcode, operand);
+    }
+
     not_yet_implemented()
 }
 
