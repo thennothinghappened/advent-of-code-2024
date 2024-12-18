92f91417e62fdc23c09468a70a9e152ac5062ad5
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2024-12-18 19:10:07 +1100

    feat: d18p1 impl Dijkstra's algorithm

--- src/days/day18.rs
+++ src/days/day18.rs
@@ -1,8 +1,11 @@
 use itertools::Itertools;
+use rustc_hash::{FxHashMap, FxHashSet};
 
 use crate::utils::{
-    boxdraw, not_yet_implemented,
-    pos::{Index2d, Pos},
+    boxdraw,
+    direction::DIRECTIONS,
+    not_yet_implemented,
+    pos::{self, Index2d, Pos},
 };
 
 use super::{DayResult, PartResult};
@@ -49,6 +52,61 @@ fn part1(input: &str) -> PartResult {
         boxdraw::draw_shape_outline(GRID_WIDTH, GRID_HEIGHT, |pos| {
             *passibility_grid.get_2d_unchecked(pos)
         })
+    );
+
+    // Set of all nodes, and their associated distances from the starting node.
+    let mut nodes = FxHashMap::<Pos, u64>::default();
+
+    // Set of nodes we haven't yet visited.
+    let mut unvisited = FxHashSet::<Pos>::default();
+
+    for y in 0..GRID_HEIGHT {
+        for x in 0..GRID_WIDTH {
+            let pos = Pos::new_from_usize_unchecked(x, y);
+
+            if *passibility_grid.get_2d_unchecked(pos) {
+                nodes.insert(pos, u64::MAX);
+                unvisited.insert(pos);
+            }
+        }
+    }
+
+    // The starting node is of course, a distance of 0 from itself!
+    nodes.entry(START).and_modify(|distance| *distance = 0);
+
+    while !unvisited.is_empty() {
+        // Choose the closest node to evaluate from.
+        let Some((&check_pos, &check_pos_dist)) = unvisited
+            .iter()
+            .map(|pos| nodes.get_key_value(pos).unwrap())
+            .filter(|&(_, dist)| *dist < u64::MAX)
+            .sorted_by(|a, b| Ord::cmp(a.1, b.1))
+            .next()
+        else {
+            break;
+        };
+
+        for neighbour_pos in DIRECTIONS
+            .iter()
+            .map(|&direction| check_pos + direction)
+            .filter(|pos| unvisited.contains(pos))
+        {
+            let dist_from_here = check_pos_dist + 1;
+
+            nodes.entry(neighbour_pos).and_modify(|dist| {
+                if *dist > dist_from_here {
+                    *dist = dist_from_here;
+                }
+            });
+        }
+
+        unvisited.remove(&check_pos);
+    }
+
+    println!(
+        "{:?}\n\nCost to reach exit: {:?}",
+        nodes,
+        nodes.get(&FINISH).unwrap()
     );
 
     not_yet_implemented()
